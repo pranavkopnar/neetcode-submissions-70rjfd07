98a8cd0da1a473c915ff1844e34997dd0c439ab5
pranavkopnar <100487955+pranavkopnar@users.noreply.github.com>
2026-05-09 13:02:23 +0530

    Add: min-cost-climbing-stairs - submission-5

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-5.java
@@ -0,0 +1,20 @@
+class Solution {
+    int[] cache;
+
+    public int minCostClimbingStairs(int[] cost) {
+        cache = new int[cost.length];
+        Arrays.fill(cache,-1);
+        return Math.min(dfs(cost,0), dfs(cost, 1));
+    }
+
+    public int dfs(int[] cost, int i) {
+        if (i >= cost.length) {
+            return 0;
+        }
+
+        if (cache[i] != -1)
+            return cache[i];
+
+        return cache[i] = cost[i] + Math.min(dfs(cost, i + 1), dfs(cost, i + 2));
+    }
+}
