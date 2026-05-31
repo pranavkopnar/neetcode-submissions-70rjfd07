7f59c317853aedf5aff80a99b738e5bf693a030d
pranavkopnar <100487955+pranavkopnar@users.noreply.github.com>
2026-05-31 18:28:47 +0530

    Add: lemonade-change - submission-0

---
+++ Data Structures & Algorithms/lemonade-change/submission-0.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        
+        int five = 0, ten = 0;
+
+        for (int b : bills) {
+            if (b == 5) {
+                five++;
+            } else if (b == 10) {
+                five--;
+                ten++;
+            } else if (ten > 0) {
+                ten--;
+                five--;
+            } else {
+                five -= 3;
+            }
+
+            if (five < 0)
+                return false;
+        }
+
+        return true;
+    }
+}
